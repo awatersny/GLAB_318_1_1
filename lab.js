cbc0649e9920bf50e3aa2c6ea9145a8a833b524c
awatersny <awatersny@gmail.com>
2025-04-03 13:23:51 -0400

    More examples

--- lab.js
+++ lab.js
@@ -1,10 +1,18 @@
 const http = require("http")
 const hostname = '127.0.0.1'
 const port = 3000
+const sthChars = [
+  {
+    name: "Sonic",
+    species: "hedgehog",
+    color: "blue"
+  }
+]
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-type", "text/plain")
-  res.write("No one expects the spanish inquisition!\n")
+  res.write('<h1 style="color: red">No one expects the spanish inquisition!</h1>');
+  res.write('<p>I wonder what else we can send...</p>');
   res.end()
 })
 
