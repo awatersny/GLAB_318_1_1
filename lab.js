0787ff58a05d3fd208d3b9936e4f9fbb62390c09
awatersny <awatersny@gmail.com>
2025-04-03 13:28:50 -0400

    Add my own example

--- lab.js
+++ lab.js
@@ -5,14 +5,28 @@ const sthChars = [
   {
     name: "Sonic",
     species: "hedgehog",
-    color: "blue"
+    colors: ["blue"]
+  },
+  {
+    name: "Tais",
+    species: "fox",
+    colors: ["yellow", "white"]
+  },
+  {
+    name: "Knuckles",
+    species: "echidna",
+    colors: ["red", "white"]
+  },
+  {
+    name: "Shadow",
+    species: "hedgehog",
+    colors: ["black", "red", "white"]
   }
 ]
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-type", "text/plain")
-  res.write('<h1 style="color: red">No one expects the spanish inquisition!</h1>');
-  res.write('<p>I wonder what else we can send...</p>');
+  res.write(JSON.stringify(sthChars))
   res.end()
 })
 
